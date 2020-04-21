6af70da0244321dd5aff9f49863994b49145a01c
Can Ural <can.ural@outlook.com>
2020-04-21 21:27:45 +0300

    changed directories

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import ScrollToTop from "./components/ScrollToTop.jsx";
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
